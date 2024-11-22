4db1099455341fc57d9e59a5c7b7b81e8720d754
Victor Schneider do Vale <schneiderrcefet2020@gmail.com>
2024-11-21 23:41:52 -0300

    função de criação de metas

--- src/front/CriarMetas/Primeira Tela/criarMetas.js
+++ src/front/CriarMetas/Primeira Tela/criarMetas.js
@@ -2,12 +2,23 @@ import UsuarioHelper from "../../script/Usuario.js";
 import MetaHelper from "../../script/Metas.js";
 import PropostaHelper from "../../script/Proposta.js";
 
-
-
 var btn = document.getElementById('btnCriarMeta');
 
 function postarMeta() {
-    console.log('23')
+    var valorObjetivo = document.getElementById('valorObjetivo').value;
+    var titulo = document.getElementById('tituloMeta').value;
+    var descricao = document.getElementById('descricaoMeta').value;
+
+    const body = {
+        valorObjetivo: valorObjetivo,
+        valorArrecadado: 0,
+        descricao: descricao,
+        titulo: titulo,
+        idCentroCriador: 3
+    }
+
+    MetaHelper.postMeta(body);
+
 }
 
 btn.addEventListener('click', postarMeta);
