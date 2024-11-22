6fc0bfffbe9f97d1938ad75a102f4b666cb71952
Victor Schneider do Vale <schneiderrcefet2020@gmail.com>
2024-11-21 23:32:44 -0300

    crriação dos CRUDs de metas e propostas

---
+++ src/front/CriarMetas/Primeira Tela/criarMetas.js
@@ -0,0 +1,13 @@
+import UsuarioHelper from "../../script/Usuario.js";
+import MetaHelper from "../../script/Metas.js";
+import PropostaHelper from "../../script/Proposta.js";
+
+
+
+var btn = document.getElementById('btnCriarMeta');
+
+function postarMeta() {
+    console.log('23')
+}
+
+btn.addEventListener('click', postarMeta);
--- src/front/CriarMetas/Primeira Tela/testeCadastro.js
+++
@@ -1,9 +0,0 @@
-import UsuarioHelper from "../../script/Usuario.js";
-
-let btn = document.getElementById("btnid");
-
-async function postarMeta() {
-
-}
-
-btn.addEventListener('click', postarMeta);
